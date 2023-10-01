28e9823560a9fa6c7951e6daa0cc81b1798cd8ba
Jose Anderson Arruda <ferrazarrudaanderson@gmail.com>
2023-10-01 11:57:11 -0300

    refactor: script Q03

--- Activity-02/js/scripts.js
+++ Activity-02/js/scripts.js
@@ -141,7 +141,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
             row.appendChild(title);
             row.appendChild(date);
-
             filmTable.appendChild(row);
         });
     };
