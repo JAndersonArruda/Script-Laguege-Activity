eaf500aa08bca8453ff92d6382a075adf8bf4966
Jose Anderson Arruda <ferrazarrudaanderson@gmail.com>
2023-10-21 19:34:19 -0300

    view: detalhamento das estruturas e inicialização dos scripts Q1 e Q2

---
+++ Activity-03/js/scripts.js
@@ -0,0 +1,27 @@
+// Question 01.
+
+const listUl = document.querySelector("#list");
+const titleInput = document.querySelector("#title-event");
+const dateTimeInput = document.querySelector("#date-time");
+const addEventButton = document.querySelector("#addButton");
+
+addEventButton.addEventListener("click", () => {});
+
+
+
+// Question 02.
+
+const listClientUl = document.querySelector("#list-client");
+const nameInput = document.querySelector("#name");
+const bankInput = document.querySelector("#bank");
+const agencyInput = document.querySelector("#agency");
+const accountInput = document.querySelector("#account");
+const typeAccountInput = document.querySelector("#type-account");
+const addClientButton = document.querySelector("#addClient");
+
+addClientButton.addEventListener("click", () => {});
+
+
+
+// Question 03.
+
