9a83927ecc1f999d609af8c25f0058597665bde1
Jose Anderson Arruda <ferrazarrudaanderson@gmail.com>
2023-11-01 13:33:18 -0300

    feat: Q1 - salavamento de dados com array

--- Activity-03/js/scripts.js
+++ Activity-03/js/scripts.js
@@ -1,27 +1,65 @@
 // Question 01.
+document.addEventListener("DOMContentLoaded", () => {
+    const listUl = document.querySelector("#list");
+    const titleInput = document.querySelector("#title-event");
+    const dateTimeInput = document.querySelector("#date-time");
+    const addEventButton = document.querySelector("#addButton");
 
-const listUl = document.querySelector("#list");
-const titleInput = document.querySelector("#title-event");
-const dateTimeInput = document.querySelector("#date-time");
-const addEventButton = document.querySelector("#addButton");
+    const arrayEvents = [];
 
-addEventButton.addEventListener("click", () => {});
+    addEventButton.addEventListener("click", () => {
+        const title = titleInput.value;
+        const dateTime = new Date(dateTimeInput.value);
+
+        const dataEvents = {
+            name: title,
+            dateTime: dateTime
+        };
+
+        arrayEvents.push(dataEvents);
+        arrayEvents.sort((a, b) => a.dateTime - b.dateTime);
+
+        listUl.replaceChildren();
+        arrayEvents.forEach((event) => {
+            const newEvent = document.createElement("li");
+            newEvent.textContent = `${event.name} - ${event.dateTime}`;
+            listUl.appendChild(newEvent);
+            titleInput.value = "";
+        });
+    });
+});
 
 
 
 // Question 02.
-
-const listClientUl = document.querySelector("#list-client");
-const nameInput = document.querySelector("#name");
-const bankInput = document.querySelector("#bank");
-const agencyInput = document.querySelector("#agency");
-const accountInput = document.querySelector("#account");
-const typeAccountInput = document.querySelector("#type-account");
-const addClientButton = document.querySelector("#addClient");
-
-addClientButton.addEventListener("click", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+    const listClientUl = document.querySelector("#list-client");
+    const nameInput = document.querySelector("#name");
+    const bankInput = document.querySelector("#bank");
+    const agencyInput = document.querySelector("#agency");
+    const accountInput = document.querySelector("#account");
+    const typeAccountInput = document.querySelector("#type-account");
+    const addClientButton = document.querySelector("#addClient");
+    
+    addClientButton.addEventListener("click", () => {
+        const name = nameInput.value;
+        const bank = bankInput.value;
+        const agency = agencyInput.value;
+        const account = accountInput.value;
+        const typeAccount = typeAccountInput.value;
+    });
+});
 
 
 
 // Question 03.
 
+// const user = [
+//     { email: "teste@gmail.com", password: "", cookies: {}}
+// ];
+
+// if (user.cookies.agreed) {
+//     CollectData(user);
+// } else {
+//     CollectData(user);
+// }
