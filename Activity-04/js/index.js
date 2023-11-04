b0317e6c4543494a959ca197969ee4535352c3b7
Jose Anderson Arruda <ferrazarrudaanderson@gmail.com>
2023-11-04 15:30:19 -0300

    feat: adicionando estrutura de armazenamento com localStorage

--- Activity-04/js/index.js
+++ Activity-04/js/index.js
@@ -1,3 +1,33 @@
+import { addProduct, listProduct } from "./lista.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const listTable = document.querySelector("#product-list");
+    const nameInput = document.querySelector("#item-name");
+    const priceInput = document.querySelector("#item-price");
+    const addProductButton = document.querySelector("#addItem");
+    
+    const arrayProduct = JSON.parse(localStorage.getItem("products")) || [];
+    
+    addProductButton.addEventListener("click", () => {
+        if (nameInput.value && priceInput.value) {
+            const name = nameInput.value;
+            const price = parseFloat(priceInput.value);
+            
+            addProduct(arrayProduct, "02887177222929-87272", name, price, false);
+            
+            listTable.replaceChildren();
+            
+            arrayProduct.forEach((product) => {
+                listTable.appendChild(listProduct(product, arrayProduct, listTable));
+                
+                nameInput.value = "";
+                priceInput.value = "";
+            });
+        }
+    });
+});
+
+/*
 document.addEventListener("DOMContentLoaded", () => {
     const listTable = document.querySelector("#product-list");
     const nameInput = document.querySelector("#item-name");
@@ -54,3 +84,4 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+*/
